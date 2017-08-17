96e510aa26a3486b36380721c041a155c8a25412
nenu <nino_roldan@yahoo.com>
2017-08-18 05:21:18 +0900

    Initial Commit

---
+++ Basic Calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic Calendar
+//
+//  Created by Nino Rorudan on 8/18/17.
+//  Copyright © 2017 The NTMC Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
