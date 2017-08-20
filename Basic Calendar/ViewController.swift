ecaab6221c00c89857f6b515a6a21410b6a313a9
nenu <nino_roldan@yahoo.com>
2017-08-20 09:16:09 +0900

    Initial commit

--- Basic Calendar/ViewController.swift
+++ Basic Calendar/ViewController.swift
@@ -10,6 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var lblMonth: UILabel!
+    
+    //Array
+    var arrayMonths:[String] =
+    [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+    
+    //variable to keep track of current array placement
+    var placement = 0
+    
+    
+    
+    
+    
+    @IBAction func btnBack(_ sender: Any)
+    {
+        if placement != 0
+        {
+            placement -= 1
+            lblMonth.text = arrayMonths[placement]
+            
+        }
+        
+    }
+    
+    @IBAction func btnForward(_ sender: Any)
+    {
+       placement += 1
+        
+        if placement == 12
+        {
+            placement = 0
+        }
+        lblMonth.text = arrayMonths[placement]
+        
+
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
